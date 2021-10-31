14b841d07e6a3240302b011db65877735a223b41
theDowidar <a7mad.dowidar@gmail.com>
2021-10-30 21:35:34 -0500

    create Random Color Generator

---
+++ app.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('button');
+const h1 = document.querySelector('h1');
+
+button.addEventListener('click', function() {
+    const newColor = genRandColor()
+    document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor
+})
+
+const genRandColor = () => {
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
